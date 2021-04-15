0fcf1ee5945fc92ae578ff7fcf326b46c5881c20
BrunoRDS <brunords@live.com>
2021-04-14 20:12:37 -0700

    Starting MileStone 2

---
+++ cmake-build-debud-remote/CMakeFiles/5300-Giraffe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 5300-Giraffe.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/5300-Giraffe.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/st/bruizdesomocurcio/cpsc5300/5300-Giraffe/sql5300.cpp" "/home/st/bruizdesomocurcio/cpsc5300/5300-Giraffe/cmake-build-debug/CMakeFiles/5300-Giraffe.dir/sql5300.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/db6/include"
+  "/home/st/bruizdesomocurcio/sql-parser/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
