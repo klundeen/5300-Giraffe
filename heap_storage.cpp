1513ebceb81a45d8a68efa18828b51edafe56919
BrunoRDS <brunords@live.com>
2021-04-18 18:23:20 -0700

    MS2 April 18 (Slotted Page)

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -5,37 +5,88 @@
 #include <iostream>
 #include <cstring>
 #include <cstdint>
-#include "utf8.h"
 
 using namespace std;
 using std::map;
 
-
-
 int DB_BLOCK_SIZE = 4096;
 
-RecordID SlottedPage::add(const Dbt *data) {
-		if (!has_room(len(data) + 4))
-		{
-			throw new invalid_argument ("Not enough room in block");
-		}
-		num_records += 1;
-		u_int16_t id = num_records;
-		u_int16_t size = len(data);
-		end_free -= size;
-		u_int16_t loc = end_free + 1;
-		put_header();
+
+SlottedPage::SlottedPage(Dbt &block, BlockID block_id, bool is_new) : DbBlock(block, block_id, is_new) {
+    if (is_new) {
+        this->num_records = 0;
+        this->end_free = DbBlock::BLOCK_SZ - 1;
+        put_header();
+    } else {
+        get_header(this->num_records, this->end_free);
+    }
+}
+
+RecordID SlottedPage::add(const Dbt* data) {
+    if (!has_room(data->get_size()))
+        throw DbBlockNoRoomError("not enough room for new record");
+    u_int16_t id = ++this->num_records;
+    u_int16_t size = (u_int16_t) data->get_size();
+    this->end_free -= size;
+    u_int16_t loc = this->end_free + 1;
+    put_header();
+    put_header(id, size, loc);
+    memcpy(this->address(loc), data->get_data(), size);
+    return id;
 }
 
 Dbt* SlottedPage::get(RecordID record_id) {
 		u_int16_t size;
 		u_int16_t loc;
-		get_header(&size, &loc, record_id);
+		get_header(size, loc, record_id);
 		if (loc == 0)
 		{
-			return NULL;
+			return nullptr;
 		}
-		return block[loc:loc + size]; //python code
+		Dbt* data = new Dbt();
+        memcpy(data->get_data(), address(loc), size);
+		return data;
+}
+
+void SlottedPage::put(RecordID record_id, const Dbt &data) {
+    u_int16_t size;
+    u_int16_t loc;
+    this->get_header(size,loc, record_id);
+    u_int32_t newSize = data.get_size();
+    if (newSize > size) {
+        u_int16_t extra = newSize - size;
+        if (!has_room(extra)) {
+            throw new DbRelationError("Not enough room in block");
+        }
+        slide(loc + newSize, loc + size);
+        memcpy(address(loc - extra), data.get_data(), newSize);
+    } else {
+        memcpy(address(loc), data.get_data(), newSize);
+        slide(loc + newSize, loc + size);
+    }
+    get_header(size, loc, record_id);
+    put_header(record_id, newSize, loc);
+}
+
+void SlottedPage::del(RecordID record_id) {
+    u_int16_t size;
+    u_int16_t loc;
+    get_header(size, loc, record_id);
+    put_header(record_id);
+    slide(loc, loc + size);
+}
+
+RecordIDs* SlottedPage::ids(void) {
+    RecordIDs* records = new RecordIDs();
+    for (int i = 1; i < num_records + 1; i++) {
+        u_int16_t size;
+        u_int16_t loc;
+        get_header(size, loc, i);
+        if (size != 0) {
+            records->push_back((RecordID)i);
+        }
+    }
+    return records;
 }
 
 void SlottedPage::get_header(u_int16_t &size, u_int16_t &loc, RecordID id) {
@@ -43,16 +94,54 @@ void SlottedPage::get_header(u_int16_t &size, u_int16_t &loc, RecordID id) {
 		loc = get_n(4 * id + 2);
 }
 
-
 void SlottedPage::put_header(RecordID id, u_int16_t size, u_int16_t loc) {
-    if (size == ){
-
+    if (id == 0) { // called the put_header() version and using the default params
+        size = this->num_records;
+        loc = this->end_free;
     }
+    put_n(4*id, size);
+    put_n(4*id + 2, loc);
 }
+
+bool SlottedPage::has_room(u_int16_t size) {
+    u_int16_t available = end_free - (num_records + 1) * 4;
+    return size <= available;
+}
+
+void SlottedPage::slide(u_int16_t start, u_int16_t end) {
+    u_int16_t shift = end - start;
+    if (shift == 0) {
+        return;
+    }
+    memcpy(address(end_free + 1 + shift), address(end_free + 1), start - end_free + 1);
+    for(RecordID id: *ids()) {
+        u_int16_t size;
+        u_int16_t loc;
+        get_header(size, loc, id);
+        if(loc <= start) {
+            loc += shift;
+            put_header(id, size, loc);
+        }
+    }
+    end_free += shift;
+    put_header();
+}
+
 
 u_int16_t SlottedPage::get_n(u_int16_t offset) {
-    return int.from_bytes(this.block[offset:offset + 2], this.byteorder=self.BYTE_ORDER) //python code
+    return *(u_int16_t *)this->address(offset);
 }
+
+void SlottedPage::put_n(u_int16_t offset, u_int16_t n) {
+    *(u_int16_t *)this->address(offset) = n;
+}
+
+void* SlottedPage::address(u_int16_t offset) {
+    return (void*)((char*)this->block.get_data() + offset);
+}
+// **************
+// * HEAP TABLE *
+// **************
 
 HeapTable::HeapTable(Identifier table_name, ColumnNames column_names,
                                ColumnAttributes column_attributes) : DbRelation(table_name, column_names, column_attributes) {
@@ -209,9 +298,9 @@ ValueDict* HeapTable::unmarshal(Dbt* data) {
     uint col_num = 0;
     for (auto const& column_name: this->column_names) {
         ColumnAttribute ca = this->column_attributes[col_num++];
-        string dataString = *(data->data);
+        string* dataString = (string*)(data->get_data());
         if (ca.get_data_type() == ColumnAttribute::DataType::INT) {
-            string subDataString = dataString.substr(offset, offset + sizeof(int32_t)); // from offset => offset + sizeof(int32_t);
+            string subDataString = dataString->substr(offset, offset + sizeof(int32_t)); // from offset => offset + sizeof(int32_t);
             const void* subDataPointer = &subDataString;
             cout << subDataString;
             int v;
@@ -219,12 +308,12 @@ ValueDict* HeapTable::unmarshal(Dbt* data) {
             Value* val = new Value(v);
             retRow->insert(pair<Identifier , Value>(column_name, *val));
         } else if (ca.get_data_type() == ColumnAttribute::DataType::TEXT) {
-            string sizeDataString = dataString.substr(offset, offset + sizeof(u_int16_t)); // Make Constant for 2
+            string sizeDataString = dataString->substr(offset, offset + sizeof(u_int16_t)); // Make Constant for 2
             const void* sizeDataPointer = &sizeDataString;
             int size;
             memcpy(&size, sizeDataPointer, sizeof(u_int16_t));
             offset += 2;
-            string subDataString = dataString.substr(offset, offset + size);
+            string subDataString = dataString->substr(offset, offset + size);
             const void* subDataPointer = &subDataString;
             cout << subDataString;
             char* v;
